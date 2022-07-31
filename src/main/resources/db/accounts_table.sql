42c4742a9bb0e2e136b969e51cd5b4416be22933
nemanovivan <nemanovid@yandex.ru>
2022-08-01 01:57:48 +0300

    Account Service

--- src/main/resources/db/accounts_table.sql
+++ src/main/resources/db/accounts_table.sql
@@ -1,14 +1,6 @@
-CREATE DATABASE accounts;
-CREATE TABLE accounts_table(
-ID int NOT NULL PRIMARY KEY,
-AMOUNT int NOT NULL,
+CREATE TABLE accounts(
+    id int NOT NULL PRIMARY KEY,
+    amount int NOT NULL
 );
-INSERT INTO accounts_table VALUES
-(1, 0),
-(2, 0),
-(3, 0),
-(4, 0),
-(5, 0),
-(6, 0),
-(7, 0),
-(8, 0);
+INSERT INTO accounts_table(id, amount)
+    VALUES (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0);
